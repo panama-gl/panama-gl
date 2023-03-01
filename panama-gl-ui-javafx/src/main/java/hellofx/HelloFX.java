7ae03117ba1fb08daa583f9978ac1d36a5dccbf9
Martin Pernollet <martin.pernollet@bioserenity.com>
2023-03-01 10:32:02 +0100

    fix copyright

--- panama-gl-ui-javafx/src/main/java/hellofx/HelloFX.java
+++ panama-gl-ui-javafx/src/main/java/hellofx/HelloFX.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2019 Gluon All rights reserved.
+ * Copyright (c) 2019, 2023 Gluon All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without modification, are permitted
  * provided that the following conditions are met: * Redistributions of source code must retain the
