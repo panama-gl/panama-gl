c1cf7414f025463a284c4754430337dee6011eb3
martin <martin@jzy3d.org>
2023-03-16 13:50:42 +0100

    try

--- panama-gl-wrappers-windows/src/main/java/panamagl/platform/windows/LoadGLExtOnWindows.java
+++ panama-gl-wrappers-windows/src/main/java/panamagl/platform/windows/LoadGLExtOnWindows.java
@@ -1,16 +1,20 @@
 package panamagl.platform.windows;
 
 import java.lang.foreign.MemoryAddress;
+import glext.ubuntu.v20.glext_h;
 import panamagl.utils.ForeignMemoryUtils;
 import wgl.windows.x86.PROC;
 import wgl.windows.x86.wgl_h;
+import wglext.windows.x86.wglext_h;
+
+import opengl.ubuntu.v20.PFNGLGENFRAMEBUFFERSEXTPROC;
 
 // Experiment something similar to FBO_linux
 //--enable-preview -Djava.library.path="C:\Windows\System32"
 public class LoadGLExtOnWindows {
 
   public static void main(String[] args) {
-    System.loadLibrary("opengl32");
+    //System.loadLibrary("opengl32");
     
     
     ForeignMemoryUtils ffm = new ForeignMemoryUtils();
@@ -20,7 +24,7 @@ public class LoadGLExtOnWindows {
     // Need a more accurate data type to access the function datatype
     // We should get an equivalent of PFNGLGENFRAMEBUFFERSEXTPROC (linux)
     // which should be named pfnWgl*
-    PROC glGenFramebuffers = PROC.ofAddress(address, ffm.getScope());
+    PFNGLGENFRAMEBUFFERSEXTPROC glGenFramebuffers = PFNGLGENFRAMEBUFFERSEXTPROC.ofAddress(address, ffm.getScope());
 
     //glGenFramebuffers.apply();
   }
