fa2ef4a18c3763be176913eb6af2aed638708489
martin <martin@jzy3d.org>
2023-04-12 14:50:20 +0200

    refactor

--- jextract-gl/src/main/java/jextract/gl/api/panamagl/GenerateAPI.java
+++ jextract-gl/src/main/java/jextract/gl/api/panamagl/GenerateAPI.java
@@ -1,19 +1,17 @@
 /*******************************************************************************
  * Copyright (c) 2022, 2023 Martin Pernollet & contributors.
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
+ * This library is free software; you can redistribute it and/or modify it under the terms of the
+ * GNU Lesser General Public License as published by the Free Software Foundation; either version
+ * 2.1 of the License, or (at your option) any later version.
  *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+ * This library is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY; without
+ * even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
  * Lesser General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
+ * You should have received a copy of the GNU Lesser General Public License along with this library;
+ * if not, write to the Free Software Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA
+ * 02110-1301, USA
  *******************************************************************************/
 package jextract.gl.api.panamagl;
 
@@ -43,8 +41,8 @@ import panamagl.platform.macos.x64.PlatformMatcher_macOS_x64;
 import panamagl.platform.windows.APanamaGLFactory_windows;
 
 /**
- * Generate an OpenGL API with per-platform implementations wrapping the bindings made available by JExtract.
- * <br>
+ * Generate an OpenGL API with per-platform implementations wrapping the bindings made available by
+ * JExtract. <br>
  * 
  * The API roughly takes the bellow form :
  * 
@@ -55,9 +53,18 @@ import panamagl.platform.windows.APanamaGLFactory_windows;
  *
  */
 public class GenerateAPI {
+  private static final String CLASS_NAME_AGL = "AGL";
+  private static final String CLASS_NAME_GLUT = "GLUT";
+  private static final String CLASS_NAME_GLU = "GLU";
   private static final String CLASS_BASE_NAME_GL = "GL_";
-  public static final String CLASS_BASE_NAME_PANAMA_GL_FACTORY = "PanamaGLFactory_";
-  public static String GL_PACKAGE = "panamagl.opengl";
+  private static final String CLASS_BASE_NAME_PANAMA_GL_FACTORY = "PanamaGLFactory_";
+  private static final String GL_PACKAGE = "panamagl.opengl";
+
+  String superGL = "GL";
+
+  Interf interf = new Interf();
+
+
 
   public static void main(String[] args) throws Exception {
     APILayout layout = new PanamaGLLayout();
@@ -83,197 +90,156 @@ public class GenerateAPI {
     boolean MACOS_ARM = false;
     boolean LINUX_x64 = false;
     boolean WINDOWS_x64 = true;
-    
+
     // ============================================================================
     // GL SPECIFICATION
-    
-    Interf interf = new Interf();
+
+    interf = new Interf();
     interf.packge = GL_PACKAGE;
-    interf.javaFolder = layout.getOutputFolder(new APIPlatform(OS.api)) + interf.packge.replace(".", "/") + "/";
-    
-    System.out.println("----------------------------------------------------------");
-    System.out.println("[Interfaces]");
+    interf.javaFolder =
+        layout.getOutputFolder(new APIPlatform(OS.api)) + interf.packge.replace(".", "/") + "/";
 
-    String superGL = "GL";
-    InterfaceWriter glInterfaceWriter = new InterfaceWriter(interf.packge, superGL);
-    
-    glInterfaceWriter.addExtension("GL_1");
-    glInterfaceWriter.addExtension("GL_2");
-    //glInterfaceWriter.addExtension("GL_3");
-    //glInterfaceWriter.addExtension("GL_4");
-    glInterfaceWriter.addExtension("GLU");
-    glInterfaceWriter.addExtension("GLUT");
-    
-    StringBuffer javaCode = new StringBuffer();
+    List<String> interfaceFiles = makeInterfaces();
 
-    String javaFile = interf.javaFolder + "/"+superGL+".java";
-    glInterfaceWriter.start(javaCode);
-    glInterfaceWriter.close(javaCode);    
-    glInterfaceWriter.writeTo(javaCode, javaFile);
-    
-    List<String> javaInterfacesFiles = interfGen.generateInterfaces(interf);
-    
     // ============================================================================
     // GL IMPLEMENTATION
-    
 
-    
     wrapperGen.addUnimplementedMethodsUponMissingBinding = false;
-    //wrapperGen.addUnimplementedMethodsFor = Sets.of("glDebugMessageCallback");
+    // wrapperGen.addUnimplementedMethodsFor = Sets.of("glDebugMessageCallback");
     wrapperGen.skipAlreadyBindedMethodNAME = true;
-    
-    Wrapper wrapper;
-    PlatformFactory factory;
-    
+
+
     // ========================================================
     // Configure macOS wrapper
-    
-    if(MACOS_x64) {
-      APIPlatform apiPlatform = new APIPlatform(OS.macOS, CPU.x64);
-      
-      wrapper = new Wrapper();
-      wrapper.wrapped = Set.of(opengl.macos.x86.glut_h.class/*, glext.macos.v10_15_7.glext_h.class, cgl.macos.v10_15_7.cgl_h.class*/);
-      wrapper.platform = apiPlatform.getName();
-      wrapper.accepts = new AcceptsGLMethod();
-      wrapper.className = CLASS_BASE_NAME_GL + wrapper.platform;
-      wrapper.packge = layout.getPlatformPackage(apiPlatform);
-      wrapper.setFileIn(layout.getOutputFolder(apiPlatform));
-  
-      wrapper.addImplement(interf.packge + "." + superGL);
-      wrapper.addImplement(interf.packge + ".GLU");
-      wrapper.addImplement(interf.packge + ".GLUT");
-      
-      wrapper.addExtension(interf.packge + ".AGL");
-      
-      // Write and compile
-      List<Method> extra = makeWrapper(javaInterfacesFiles, wrapper);
-      
-      makeGLUT(interf, extra);
-      
-      // Configure macOS factory
-      factory = new PlatformFactory();
-      factory.base = APanamaGLFactory_macOS.class;
-      factory.name = CLASS_BASE_NAME_PANAMA_GL_FACTORY + wrapper.platform;
-      factory.packge = wrapper.packge;
-      factory.matcher = PlatformMatcher_macOS_x64.class;
-      factory.setFileIn(layout.getOutputFolder(apiPlatform));
-      
-      makeFactory(javaInterfacesFiles, wrapper, factory);
-    }
-    
-    if(MACOS_ARM) {
-      APIPlatform apiPlatform = new APIPlatform(OS.macOS, CPU.arm);
 
-      wrapper = new Wrapper();
-      wrapper.wrapped = Set.of(/*glext.macos.x86.glext_h.class, */opengl.macos.arm.glut_h.class);
-      wrapper.platform = apiPlatform.getName();
-      wrapper.accepts = new AcceptsGLMethod();
-      wrapper.className = CLASS_BASE_NAME_GL + wrapper.platform;
-      wrapper.packge = layout.getPlatformPackage(apiPlatform);
-      wrapper.setFileIn(layout.getOutputFolder(apiPlatform));
-  
-      wrapper.addImplement(interf.packge + "." + superGL);
-      wrapper.addImplement(interf.packge + ".GLU");
-      wrapper.addImplement(interf.packge + ".GLUT");
-      
-      wrapper.addExtension(interf.packge + ".AGL");
-      
-      // Write and compile
-      List<Method> extra = makeWrapper(javaInterfacesFiles, wrapper);
-      
-      makeGLUT(interf, extra);
-      
-      // Configure macOS factory
-      factory = new PlatformFactory();
-      factory.base = APanamaGLFactory_macOS.class;
-      factory.name = CLASS_BASE_NAME_PANAMA_GL_FACTORY + wrapper.platform;
-      factory.packge = wrapper.packge;
-      factory.matcher = PlatformMatcher_macOS_arm.class;
-      factory.setFileIn(layout.getOutputFolder(apiPlatform));
-      
-      makeFactory(javaInterfacesFiles, wrapper, factory);
+    if (MACOS_x64) {
+
+      APIPlatform platform = new APIPlatform(OS.macOS, CPU.x64);
+      Set<Class<?>> wrapped = Set.of(opengl.macos.x86.glut_h.class);
+      Class<?> factoryBase = APanamaGLFactory_macOS.class;
+      Class<?> factorymatcher = PlatformMatcher_macOS_x64.class;
+      boolean genGlut = true;
+
+      makeWrapperAndFactory(layout, platform, wrapped, factoryBase, factorymatcher, genGlut,
+          interfaceFiles);
     }
-    
+
+    if (MACOS_ARM) {
+
+      APIPlatform platform = new APIPlatform(OS.macOS, CPU.arm);
+      Set<Class<?>> wrapped = Set.of(opengl.macos.arm.glut_h.class);
+      Class<?> factoryBase = APanamaGLFactory_macOS.class;
+      Class<?> factorymatcher = PlatformMatcher_macOS_arm.class;
+      boolean genGlut = true;
+
+
+      makeWrapperAndFactory(layout, platform, wrapped, factoryBase, factorymatcher, genGlut,
+          interfaceFiles);
+    }
+
     // Compile ALL
-    compile(javaInterfacesFiles);
+    compile(interfaceFiles);
 
 
     // ========================================================
     // Configure Linux wrapper
-    
-    if(LINUX_x64) {
-      APIPlatform apiPlatform = new APIPlatform(OS.linux, CPU.x64);
 
-      wrapper = new Wrapper();
-      wrapper.wrapped = Set.of(glext.ubuntu.v20.glext_h.class/*, opengl.ubuntu.v20.glut_h.class, glxext.ubuntu.v20.glxext_h.class*/);
-      wrapper.platform = apiPlatform.getName();
-      wrapper.accepts = new AcceptsGLMethod();
-      wrapper.className = CLASS_BASE_NAME_GL + wrapper.platform;
-      wrapper.packge = layout.getPlatformPackage(apiPlatform);
-      wrapper.setFileIn(layout.getOutputFolder(apiPlatform));
-      
-      wrapper.addImplement(interf.packge + "." + superGL);
-      wrapper.addImplement(interf.packge + ".GLU");
-      wrapper.addImplement(interf.packge + ".GLUT");
-      
-      wrapper.addExtension(interf.packge + ".AGL");
+    if (LINUX_x64) {
 
-
-      // Write and compile
       wrapperGen.addUnimplementedMethodsUponMissingBinding = true;
-      makeWrapper(javaInterfacesFiles, wrapper);
-  
-      // Configure Linux factory
-      factory = new PlatformFactory();
-      factory.base = APanamaGLFactory_linux.class;
-      factory.name = CLASS_BASE_NAME_PANAMA_GL_FACTORY + wrapper.platform;
-      factory.packge = layout.getPlatformPackage(apiPlatform);
-      factory.setFileIn(layout.getOutputFolder(apiPlatform));
-      
-      makeFactory(javaInterfacesFiles, wrapper, factory);
 
-    }   
-    
+      APIPlatform platform = new APIPlatform(OS.linux, CPU.x64);
+      Set<Class<?>> wrapped = Set.of(glext.ubuntu.v20.glext_h.class);
+      Class<?> factoryBase = APanamaGLFactory_linux.class;
+      Class<?> factorymatcher = null;
+      boolean genGlut = false;
+
+      makeWrapperAndFactory(layout, platform, wrapped, factoryBase, factorymatcher, genGlut,
+          interfaceFiles);
+
+    }
+
     // ========================================================
     // Configure Windows wrapper
-    
-    if(WINDOWS_x64) {
-      APIPlatform apiPlatform = new APIPlatform(OS.windows, CPU.x64);
 
-      wrapper = new Wrapper();
-      wrapper.wrapped = Set.of(freeglut.windows.x86.freeglut_h.class);
-      wrapper.platform = apiPlatform.getName();
-      wrapper.accepts = new AcceptsGLMethod();
-      wrapper.className = CLASS_BASE_NAME_GL + wrapper.platform;
-      wrapper.packge = layout.getPlatformPackage(apiPlatform);
-      wrapper.setFileIn(layout.getOutputFolder(apiPlatform));
-      
-      wrapper.addImplement(interf.packge + "." + superGL);
-      wrapper.addImplement(interf.packge + ".GLU");
-      wrapper.addImplement(interf.packge + ".GLUT");
-      
-      wrapper.addExtension(interf.packge + ".AGL");
+    if (WINDOWS_x64) {
 
-
-      // Write and compile
       wrapperGen.addUnimplementedMethodsUponMissingBinding = true;
-      makeWrapper(javaInterfacesFiles, wrapper);
-  
-      // Configure Linux factory
-      factory = new PlatformFactory();
-      factory.base = APanamaGLFactory_windows.class;
-      factory.name = CLASS_BASE_NAME_PANAMA_GL_FACTORY + wrapper.platform;
-      factory.packge = layout.getPlatformPackage(apiPlatform);
-      factory.setFileIn(layout.getOutputFolder(apiPlatform));
-      
-      makeFactory(javaInterfacesFiles, wrapper, factory);
 
-    }   
-    
+      APIPlatform platform = new APIPlatform(OS.windows, CPU.x64);
+      Set<Class<?>> wrapped = Set.of(freeglut.windows.x86.freeglut_h.class);
+      Class<?> factoryBase = APanamaGLFactory_windows.class;
+      Class<?> factorymatcher = null;
+      boolean genGlut = false;
+
+      makeWrapperAndFactory(layout, platform, wrapped, factoryBase, factorymatcher, genGlut,
+          interfaceFiles);
+
+    }
+
     // Compile ALL
-    compile(javaInterfacesFiles);
+    compile(interfaceFiles);
   }
 
+  protected List<String> makeInterfaces() throws IOException, Exception {
+    System.out.println("----------------------------------------------------------");
+    System.out.println("[Interfaces]");
+
+    InterfaceWriter glInterfaceWriter = new InterfaceWriter(interf.packge, superGL);
+
+    glInterfaceWriter.addExtension("GL_1");
+    glInterfaceWriter.addExtension("GL_2");
+    // glInterfaceWriter.addExtension("GL_3");
+    // glInterfaceWriter.addExtension("GL_4");
+    glInterfaceWriter.addExtension(CLASS_NAME_GLU);
+    glInterfaceWriter.addExtension(CLASS_NAME_GLUT);
+
+    StringBuffer javaCode = new StringBuffer();
+
+    String javaFile = interf.javaFolder + "/" + superGL + ".java";
+    glInterfaceWriter.start(javaCode);
+    glInterfaceWriter.close(javaCode);
+    glInterfaceWriter.writeTo(javaCode, javaFile);
+
+    List<String> interfaceFiles = interfGen.generateInterfaces(interf);
+    return interfaceFiles;
+  }
+
+  protected void makeWrapperAndFactory(APILayout layout, APIPlatform platform,
+      Set<Class<?>> wrapped, Class<?> factoryBase, Class<?> factorymatcher, boolean genGlut,
+      List<String> javaInterfacesFiles) throws IllegalAccessException, IOException {
+
+
+    Wrapper wrapper = new Wrapper();
+    wrapper.wrapped = wrapped;
+    wrapper.platform = platform.getName();
+    wrapper.accepts = new AcceptsGLMethod();
+    wrapper.className = CLASS_BASE_NAME_GL + wrapper.platform;
+    wrapper.packge = layout.getPlatformPackage(platform);
+    wrapper.setFileIn(layout.getOutputFolder(platform));
+
+    wrapper.addImplement(interf.packge + "." + superGL);
+    wrapper.addImplement(interf.packge + "." + CLASS_NAME_GLU);
+    wrapper.addImplement(interf.packge + "." + CLASS_NAME_GLUT);
+
+    wrapper.addExtension(interf.packge + "." + CLASS_NAME_AGL);
+
+    // Write and compile
+    List<Method> extra = makeWrapper(javaInterfacesFiles, wrapper);
+
+    if (genGlut)
+      makeGLUT(interf, extra);
+
+    // Configure macOS factory
+    PlatformFactory factory = new PlatformFactory();
+    factory.name = CLASS_BASE_NAME_PANAMA_GL_FACTORY + wrapper.platform;
+    factory.packge = layout.getPlatformPackage(platform);
+    factory.base = factoryBase;
+    factory.matcher = factorymatcher;
+    factory.setFileIn(layout.getOutputFolder(platform));
+
+    makeFactory(javaInterfacesFiles, wrapper, factory);
+  }
 
 
 
@@ -289,30 +255,30 @@ public class GenerateAPI {
     ClassWriter factoryWriter = new ClassWriter(factory.packge, factory.name);
     factoryWriter.addExtension(factory.base.getName());
     factoryWriter.addImplement(panamagl.factory.PanamaGLFactory.class.getName());
-    
-    
+
+
     StringBuffer javaCode = new StringBuffer();
     factoryWriter.start(javaCode);
 
     // -------------------------------------------------------------------
     // Create a method to instanciate the appropriate GL wrapper
-    
+
     Code c = new Code("return new " + wrapper.packge + "." + wrapper.className + "();");
     factoryWriter.method(javaCode, "newGL", null, new Arg(panamagl.opengl.GL.class.getName()), c);
-    
+
     // -------------------------------------------------------------------
     // Create a method to instanciate the appropriate platform matcher
-    
-    if(factory.matcher!=null) {
+
+    if (factory.matcher != null) {
       Arg input = new Arg(Platform.class.getName(), "platform");
       Code cn = new Code("return new " + factory.matcher.getName() + "().matches(platform);");
       factoryWriter.method(javaCode, "matches", List.of(input), new Arg("boolean"), cn);
     }
-    
-    
+
+
     factoryWriter.close(javaCode);
     factoryWriter.writeTo(javaCode, factory.javaFile);
-    
+
     javaInterfacesFiles.add(factory.javaFile);
   }
 
@@ -329,13 +295,13 @@ public class GenerateAPI {
       throws IllegalAccessException, IOException {
     List<String> cmp = new ArrayList<>(javaInterfacesFiles);
     cmp.add(wrapper.javaFile);
-    
+
     System.out.println("----------------------------------------------------------");
     System.out.println("[Wrapping] " + wrapper.wrapped + " in " + wrapper.className);
     wrapperGen.generateWrapper(wrapper);
     return wrapperGen.getAutoWrappedMethods();
   }
-  
+
   /**
    * 
    * @param interf
@@ -344,35 +310,34 @@ public class GenerateAPI {
    */
   protected void makeGLUT(Interf interf, List<Method> extra) throws IOException {
     // Create GLU et GLUT interface
-    InterfaceWriter gluWriter = new InterfaceWriter(interf.packge, "GLU");
-    InterfaceWriter glutWriter = new InterfaceWriter(interf.packge, "GLUT");
-    
+    InterfaceWriter gluWriter = new InterfaceWriter(interf.packge, CLASS_NAME_GLU);
+    InterfaceWriter glutWriter = new InterfaceWriter(interf.packge, CLASS_NAME_GLUT);
+
     gluWriter.addImport("java.lang.foreign.*");
     glutWriter.addImport("java.lang.foreign.*");
-    
+
     String glutFile = interf.javaFolder + "/GLUT.java";
     String gluFile = interf.javaFolder + "/GLU.java";
 
-    StringBuffer glutCode= new StringBuffer();
-    StringBuffer gluCode= new StringBuffer();   
-    
+    StringBuffer glutCode = new StringBuffer();
+    StringBuffer gluCode = new StringBuffer();
+
     glutWriter.start(glutCode);
     gluWriter.start(gluCode);
-    for(Method method: extra) {
-      if(method.getName().startsWith("glut") && ! method.getName().contains("$")) {
+    for (Method method : extra) {
+      if (method.getName().startsWith("glut") && !method.getName().contains("$")) {
         glutWriter.method(glutCode, method);
-      }
-      else if(method.getName().startsWith("glu") && ! method.getName().contains("$")) {
+      } else if (method.getName().startsWith("glu") && !method.getName().contains("$")) {
         gluWriter.method(gluCode, method);
       }
     }
     glutWriter.close(glutCode);
     gluWriter.close(gluCode);
-    
+
     glutWriter.writeTo(glutCode, glutFile);
     gluWriter.writeTo(gluCode, gluFile);
   }
-  
+
   public void compile(List<String> javaFile) throws IOException {
     ClassCompiler c = new ClassCompiler();
     c.compile(javaFile);
