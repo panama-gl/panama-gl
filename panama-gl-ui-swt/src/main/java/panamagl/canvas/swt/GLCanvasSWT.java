4b143b2db2c66991c04ddf9e41ad9333f8533c0b
Martin Pernollet <martin.pernollet@doctrine.fr>
2026-05-03 20:26:41 +0200

    clean

--- panama-gl-ui-swt/src/main/java/panamagl/canvas/swt/GLCanvasSWT.java
+++ panama-gl-ui-swt/src/main/java/panamagl/canvas/swt/GLCanvasSWT.java
@@ -1,9 +1,5 @@
 /*******************************************************************************
-<<<<<<< HEAD
  * Copyright (c) 2023 Martin Pernollet and others
-=======
- * Copyright (c) 2023 Christoph Läubrich, Martin Pernollet and others
->>>>>>> 91b68a7 (.)
  *
  * This program and the accompanying materials are made available under the terms of the Eclipse
  * Public License 2.0 which is available at http://www.eclipse.org/legal/epl-2.0.
